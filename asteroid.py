8a28962e67aa9a7ce0efa92008e24e7ecd76ee78
spawned18 <spawnedlive@gmail.com>
2024-10-03 16:03:20 -0500

    added asteroids and spawning mechanisms

---
+++ asteroid.py
@@ -0,0 +1,25 @@
+# type: ignore
+import pygame
+from circleshape import CircleShape
+
+# Base class for game objects
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(
+        screen,
+        "white",
+        (self.x, self.y),
+        self.radius,
+        2
+    )
+
+    def update(self, dt):
+        self.x += (self.velocity.x * dt)
+        self.y += (self.velocity.y * dt)
