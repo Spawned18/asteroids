6559c6ff686d32724faf8eabfcc1ea1166f57d04
spawned18 <spawnedlive@gmail.com>
2024-10-02 20:00:37 -0500

    the player can now move forwards/backwards

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 # type: ignore
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -31,6 +31,14 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+            self.rotate(-dt) # rotate left
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotate(dt) # rotate right
+        if keys[pygame.K_s]:
+            self.move(-dt) # move backwards
+        if keys[pygame.K_w]:
+            self.move(dt) # move forwards
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
